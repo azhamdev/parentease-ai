62a045a258ec4d3f5cfa8adbdb17baa98730400f
azizarwn <azzahrawani98@gmail.com>
2026-05-09 15:36:47 +0700

    feat : session, redis, & gitignore

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel
 from scalar_fastapi import get_scalar_api_reference
 from .models import create_db_and_tables, ChatMessage, engine
 from .agent import process_parent_query
+from app.src.api.v1.endpoints import sessions
+from fastapi.middleware.cors import CORSMiddleware
 
 STATIC_DIR = pathlib.Path(__file__).parent / "static"
 
@@ -19,6 +21,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="ParentEase AI Backend", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Ganti ["http://localhost:3000"] di production
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(sessions.router)
 
 
 class ChatRequest(BaseModel):
---
+++ app/src/api/v1/endpoints/sessions.py
@@ -0,0 +1,13 @@
+from app.src.utils.schemas import SessionCreate
+from fastapi import APIRouter, Depends, HTTPException
+from app.src.services.agent.orchestrator import create_session
+
+router = APIRouter(prefix="/sessions", tags=["sessions"])
+
+@router.post("/")
+async def init_session(payload: SessionCreate):
+    try:
+        session_id, redis_key = await create_session(payload)
+        return {"session_id": session_id, "status": "initialized"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Session init failed: {str(e)}")
